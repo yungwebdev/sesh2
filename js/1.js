0eeeeb1d4fb83c10c861b115657c666bcf6173b6
bear <joabarzua@yahoo.com>
2016-11-10 20:34:09 -0500

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -5,7 +5,7 @@ $(function() {
 	for (i = 0; i < 3; i++) {
 		$('.buzz_wrapper .text span').eq(0).clone().prependTo('.buzz_wrapper .text');
 	}
-	for (i = 0; i < 100; i++) {
+	for (i = 0; i < 50; i++) {
 		$('.buzz_wrapper .scanline').eq(0).clone().appendTo('.buzz_wrapper');
 	}
 });
