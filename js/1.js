a40b762c0d3bcc011dd552e738f1047f50c4ae9e
bear <joabarzua@yahoo.com>
2016-11-10 19:40:30 -0500

    Create 1.js

---
+++ js/1.js
@@ -0,0 +1,8 @@
+$(function() {
+	for (i = 0; i < 3; i++) {
+		$('.buzz_wrapper .text span').eq(0).clone().prependTo('.buzz_wrapper .text');
+	}
+	for (i = 0; i < 100; i++) {
+		$('.buzz_wrapper .scanline').eq(0).clone().appendTo('.buzz_wrapper');
+	}
+});
